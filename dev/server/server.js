98ce19a69d81762a48456388fde41cc99970e8ee
Denis C de Azevedo <denis.azevedo@gmail.com>
2014-07-25 23:50:13 -0300

    add users and lists

--- dev/server/server.js
+++ dev/server/server.js
@@ -4,12 +4,12 @@ var morgan      =   require('morgan');
 var db          =   mongojs('bucketlistapp', ['appUsers','bucketLists']);
 var server      =   restify.createServer();
 
-erver.use(restify.acceptParser(server.acceptable));
+server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 server.use(morgan('dev')); // LOGGER
 
-// CORS
+// CORS (Cross Origin Request Sharing)
 server.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -20,3 +20,6 @@ server.use(function(req, res, next) {
 server.listen(process.env.PORT || 9804, function () {
     console.log("Server started @ ",process.env.PORT || 9804);
 });
+
+var manageUsers = require('./auth/manageUser')(server, db);
+var manageLists = require('./list/manageList')(server, db);
